71e7ccdfaa6c394768f60bb106fbe47121ce7e56
Tushar Katore <144722798+tusharRkatore@users.noreply.github.com>
2025-12-09 06:00:24 +0530

    Add CSS import to Dashboard component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -3,14 +3,17 @@ import TransactionsPage from "../components/TransactionPage";
 import StatisticsPage from "../components/StatisticsPage";
 import PriceDistributionChart from "../components/PriceDistributionChart";
 import CategoryDistributionChart from "../components/CategoryDistributionChart";
+import "./Dashboard.css";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="dashboard-container">
+    <div className="dashboard-grid">
       <TransactionsPage />
       <StatisticsPage />
       <PriceDistributionChart />
       <CategoryDistributionChart />
+    </div>
     </div>
   );
 }
